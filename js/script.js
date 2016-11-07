da873656220832e99e34a44d780b1309357fbaab
Accr <574150795@qq.com>
2016-11-07 22:47:02 +0800

    version 0.1
    set loading style

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/**
+ * Created by AC on 2016/10/25.
+ */
+
+$(window).load(function() {
+    //Preload
+    $('.dots-loader').delay(500).fadeOut();
+    $('#preload').delay(500).fadeOut('slow');
+    $('body').delay(1000).css({'overflow':'visible'});
+})
+
+
